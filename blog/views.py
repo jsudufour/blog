fa8100bfafde06a1e544861353c520c117a2dd87
jsudufour <jsudufour@gmail.com>
2017-05-28 20:38:09 -0400

    Add first attempt at selecting a number of entries from a dropdown to display per page

--- blog/views.py
+++ blog/views.py
@@ -102,3 +102,62 @@ def delete_entry_post(id):
     session.delete(entry)
     session.commit()
     return redirect(url_for("entries"))
+
+@app.route("/?limit=<dropdown>")
+def entry_limit(dropdown):
+
+    PAGINATE_BY = dropdown
+
+    # Zero-indexed page
+    page_index = 0
+
+    count = session.query(Entry).count()
+
+    start = page_index * PAGINATE_BY
+    end = start + PAGINATE_BY
+
+    total_pages = (count - 1) // PAGINATE_BY + 1
+    has_next = page_index < total_pages - 1
+    has_prev = page_index > 0
+
+    entries = session.query(Entry)
+    entries = entries.order_by(Entry.datetime.desc())
+    entries = entries[start:end]
+    print("entries # " + start)
+
+    return render_template("entries.html",
+        entries=entries,
+        has_next=has_next,
+        has_prev=has_prev,
+        page=page,
+        total_pages=total_pages
+    )
+
+@app.route("/page/<int:page>/?limit=<dropdown>")
+def entry_limit_page(dropdown, page=1):
+    # dropdown = request.form['entries-per-page']
+    PAGINATE_BY = dropdown
+
+    # Zero-indexed page
+    page_index = page - 1
+
+    count = session.query(Entry).count()
+
+    start = page_index * PAGINATE_BY
+    end = start + PAGINATE_BY
+
+    total_pages = (count - 1) // PAGINATE_BY + 1
+    has_next = page_index < total_pages - 1
+    has_prev = page_index > 0
+
+    entries = session.query(Entry)
+    entries = entries.order_by(Entry.datetime.desc())
+    entries = entries[start:end]
+
+    return render_template("entries.html",
+        entries=entries,
+        has_next=has_next,
+        has_prev=has_prev,
+        page=page,
+        total_pages=total_pages
+    )
