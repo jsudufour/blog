478dea4b26f6a8606960a07cd15fca56721ca211
jsudufour <jsudufour@gmail.com>
2017-05-30 20:17:06 -0400

    Add a login form and authentication

--- blog/database.py
+++ blog/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from . import app
 import datetime
 from sqlalchemy import Column, Integer, String, Text, DateTime
+from flask_login import UserMixin
 
 engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
 Base = declarative_base()
@@ -17,5 +18,13 @@ class Entry(Base):
     title = Column(String(1024))
     content = Column(Text)
     datetime = Column(DateTime, default=datetime.datetime.now)
+
+class User(Base, UserMixin):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+    email = Column(String(128), unique=True)
+    password = Column(String(128))
 
 Base.metadata.create_all(engine)
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from flask import render_template
-
 from . import app
-from .database import session, Entry
-from flask import request, redirect, url_for
+from .database import session, Entry, User
+from flask import request, redirect, url_for, flash
+from flask_login import login_user
+from werkzeug.security import check_password_hash
 
 PAGINATE_BY = 10
 
@@ -112,3 +113,19 @@ def delete_entry_post(id):
     session.delete(entry)
     session.commit()
     return redirect(url_for("entries"))
+
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("entries"))
