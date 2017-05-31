70294d54b02d2f2e77650fe7ee9ad7c3be27f23b
jsudufour <jsudufour@gmail.com>
2017-05-31 11:11:28 -0400

    Tests for unit test, acceptance test and integration test to test filter, adding a blog entry and logging in

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -12,5 +12,14 @@ from blog.filters import *
 class FilterTests(unittest.TestCase):
     pass
 
+    def test_date_format(self):
+        date = datetime.date(1999, 12, 31)
+        formatted = dateformat(date, "%y/%m/%d")
+        self.assertEqual(formatted, "99/12/31")
+
+    def test_date_format_none(self):
+        formatted = dateformat(None, "%y/%m/%d")
+        self.assertEqual(formatted, None)
+
 if __name__ == "__main__":
     unittest.main()
