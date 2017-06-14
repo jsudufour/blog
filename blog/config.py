a53ef4ac1fafdebb8d4a0c322ce93b1aef00d528
jsudufour <jsudufour@gmail.com>
2017-06-13 22:23:02 -0400

    Debug Travis CI

--- blog/config.py
+++ blog/config.py
@@ -6,11 +6,11 @@ class DevelopmentConfig(object):
     SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", os.urandom(12))
 
 class TestingConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://thinkful:thinkful@localhost:5432/blogful-test"
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/blogful-test"
     DEBUG = False
     SECRET_KEY = "Not secret"
 
 class TravisConfig(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:@localhost:5432/blogful-test"
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost:5432/blogful-test"
     DEBUG = False
     SECRET_KEY = "Not secret"
